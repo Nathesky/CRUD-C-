7cbf1ca22229a4ae64535a861f65647bb18082d5
Nathesky <132522541+Nathesky@users.noreply.github.com>
2023-12-13 19:19:55 -0300

    Add files via upload

--- database/agenda.sql
+++ database/agenda.sql
@@ -10,3 +10,12 @@ create table tbcontato(
 );
 
 insert into tbcontato(nome, telefone, celular, email) values ('João Botão', '(11) 4002-8922', '(11) 98492-9475', 'botaojoao@gmail.com');
+
+create table tblogin(
+	login varchar(30) not null primary key
+	senha varchar(30) not null
+);
+
+insert into tblogin(login, senha) values ('adm', '123');
+
+select * from tblogin;
